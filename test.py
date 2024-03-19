1f3eafae5e50f1ee0cfc100e218f3757bd62e8ce
sidi <486772+sidi@users.noreply.github.com>
2024-03-19 11:36:02 +0000

    test conflict 5

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import re
 
+filename = 'conflicted_file.txt'
+with open(filename, 'w') as f:
+    f.write('Some combined content which resolves the merge issue')
+# repo is a git.Repo instance
+repo.index.add([filename])
+repo.index.commit("Simulating user resolving a conflict"+filename)
+
 def fff():
 	aa=0
 	bba=aa+1
